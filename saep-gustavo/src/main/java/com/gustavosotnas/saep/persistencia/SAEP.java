89761342020324f41fee050f07f8c79ca8dc33c1
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-10 14:05:10 -0300

    Criada classe main para teste

---
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/SAEP.java
@@ -0,0 +1,10 @@
+package com.gustavosotnas.saep.persistencia;
+
+/**
+ * Created by gustavosotnas on 10/07/16.
+ */
+public class SAEP {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
