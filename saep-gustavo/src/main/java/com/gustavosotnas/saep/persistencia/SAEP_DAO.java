74452d320e0d49cf2233632cd1d196366825aa53
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-11 07:10:25 -0300

    Criada "enum" de Strings (nomes de collections)

--- saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/SAEP_DAO.java
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/SAEP_DAO.java
@@ -10,17 +10,13 @@ import com.mongodb.client.MongoDatabase;
 public class SAEP_DAO {
     public static MongoClient mongoClient =  new MongoClient();
     public static MongoDatabase db = mongoClient.getDatabase("saep");
-    public static String PARECER_COLLECTION = "parecer_collection";
-    public static String RADOC_COLLECTION = "radoc_collection";
-    public static String RESOLUCAO_COLLECTION = "resolucao_collection";
 
     public static void createCollections(){
         try{
-            db.createCollection(SAEP_DAO.PARECER_COLLECTION);
-            db.createCollection(SAEP_DAO.RESOLUCAO_COLLECTION);
-        }catch(MongoCommandException e ){
+            db.createCollection(Strings.PARECER_COLLECTION);
+            db.createCollection(Strings.RESOLUCAO_COLLECTION);
+        } catch (MongoCommandException e){
             System.out.println("Collections já criadas");
         }
-
     }
 }
---
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/Strings.java
@@ -0,0 +1,12 @@
+package com.gustavosotnas.saep.persistencia;
+
+/**
+ * Created by gustavosotnas on 11/07/16.
+ */
+// "enum" de strings no SAEP
+public class Strings {
+
+    public static final String PARECER_COLLECTION = "parecer_collection";
+    public static final String RADOC_COLLECTION = "radoc_collection";
+    public static final String RESOLUCAO_COLLECTION = "resolucao_collection";
+}
