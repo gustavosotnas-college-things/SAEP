8f92b4c4507f1bf830afd43cd894402f043896de
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-11 15:49:10 -0300

    Feito um pequeno header para o início do programa

--- saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/view/SAEP.java
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/view/SAEP.java
@@ -7,7 +7,8 @@ import com.gustavosotnas.saep.persistencia.controller.DBController;
  */
 public class SAEP {
     public static void main(String args[]) {
+        System.out.println("Persistência do Sistema de Apoio à Elaboração de Pareceres (SAEP)\n" +
+                "por Gustavo Moraes (http://github.com/gustavosotnas)");
         DBController.initializeDB();
-        System.out.println("Hello world");
     }
 }
