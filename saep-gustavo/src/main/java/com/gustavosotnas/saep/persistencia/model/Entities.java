52435d8bf22160715bcafaad464e329e1a4bee92
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-11 17:15:04 -0300

    Criado "enum" de nomes de entidades no SAEP

---
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/model/Entities.java
@@ -0,0 +1,11 @@
+package com.gustavosotnas.saep.persistencia.model;
+
+/**
+ * Created by gustavosotnas on 11/07/16.
+ */
+// "enum" de entidades do banco de dados do SAEP
+public class Entities {
+
+    public static final String RADOC_ENTITY = "radoc";
+    public static final String PARECER_ENTITY = "parecer";
+}
