439fb1253fb315129e5a7a486d7b91a16f0244b2
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-12 15:18:46 -0300

    Implementadas operações mais avançadas com o banco de dados.

--- saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/controller/DBController.java
+++ saep-gustavo/src/main/java/com/gustavosotnas/saep/persistencia/controller/DBController.java
@@ -8,10 +8,15 @@ import com.mongodb.MongoTimeoutException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import java.util.regex.Pattern;
+
 /**
+ * Classe que reúne funções úteis para manipular o banco de dados MongoDB no SAEP.
+ *
  * Created by gustavosotnas on 10/07/16.
  */
 public class DBController {
@@ -38,14 +43,17 @@ public class DBController {
     }
 
     /**
-     * Salva um objeto Java serializado para JSON em uma collection.
+     * Salva um documento em uma collection no banco de dados.
      *
-     * @param jsonObject o JSON correspondente à uma classe DAO do SAEP.
+     * @implNote Se não quiser o Document criado de retorno, simplesmente ignore
+     * o retorno não setando o mesmo para variável alguma.
+     *
+     * @param JSONdocument o JSON correspondente à uma classe DAO do SAEP.
      * @param collectionName o nome da collection que se deseja salvar.
      */
-    public static Document setCollection(String jsonObject, String collectionName) {
+    public static Document setCollection(String JSONdocument, String collectionName) {
         MongoCollection<Document> collection = getCollection(collectionName);
-        Document documentToSave = parseJsonToDocument(jsonObject);
+        Document documentToSave = parseJSON(JSONdocument);
         collection.insertOne(documentToSave);
         return findDocumentById((ObjectId) documentToSave.get("_id"), collectionName);
     }
@@ -53,18 +61,18 @@ public class DBController {
     /**
      * Faz busca de um objeto de uma collection pelo seu nome (id).
      *
-     * @param idName nome do atributo no banco de dados desejado
-     * @param idValue valor do atributo que será procurado no banco de dados.
+     * @param atributeName nome do atributo no banco de dados desejado
+     * @param atributeValue valor do atributo que será procurado no banco de dados.
      * @param collectionName nome da collection desejada a se obter.
      * @return a collection desejada.
      */
-    public static Document findDocumentById(String idName, String idValue, String collectionName) {
+    public static Document findDocument(String atributeName, String atributeValue, String collectionName) {
         MongoCollection<Document> collection = getCollection(collectionName);
-        return collection.find(eq(idName, idValue)).first();
+        return collection.find(eq(atributeName, atributeValue)).first();
     }
 
     /**
-     * Faz busca de um objeto de uma collection por uma query no formato Document.
+     * Faz busca de um Document de uma collection por uma query.
      *
      * @param collectionName nome da collection desejada a se obter.
      * @param query document de padrão para pesquisa no banco de dados.
@@ -81,9 +89,64 @@ public class DBController {
      * @param collectionName nome da collection desejada a se obter.
      * @return um conjunto de Documents da collection desejada.
      */
-    public Iterable<Document> getAllDocuments(String collectionName) {
+    public static Iterable<Document> getAllDocuments(String collectionName) {
         MongoCollection<Document> collection = getCollection(collectionName);
         return collection.find();
+    }
+
+    /**
+     * Obtêm Document(s) que possuem valor igual ou parecido ao desejado.
+     *
+     * @param atributeName nome do atributo no banco de dados desejado.
+     * @param atributeValue valor do atributo que será procurado no banco de dados.
+     * @param collectionName nome da collection desejada a se obter.
+     * @return Um ou mais Documents que batem com o desejado.
+     */
+    public static Iterable<Document> getDocumentsSimilarTo(String atributeName, String atributeValue, String collectionName) {
+        MongoCollection<Document> collection = getCollection(collectionName);
+        Document query = new Document(atributeName, Pattern.compile(atributeValue));
+        return collection.find(query);
+    }
+
+    /**
+     * Atualiza um Document em uma collection existente no banco de dados (formato JSON).
+     *
+     * @param atributeName nome do atributo no banco de dados desejado.
+     * @param atributeValue valor do atributo que será atualizado no banco de dados.
+     * @param collectionName nome da collection a ser atualizada.
+     * @param JSONdocument o conteúdo do document a ser atualizado.
+     */
+    public static void updateDocument(String atributeName, String atributeValue, String collectionName, String JSONdocument) {
+        MongoCollection<Document> collection = getCollection(collectionName);
+        Document documentToUpdate = parseJSON(JSONdocument);
+        collection.replaceOne(eq(atributeName, atributeValue), documentToUpdate);
+    }
+
+    /**
+     * Atualiza um Document em uma collection existente no banco de dados (formato Document).
+     *
+     * @param atributeName nome do atributo no banco de dados desejado.
+     * @param atributeValue valor do atributo que será atualizado no banco de dados.
+     * @param collectionName nome da collection a ser atualizada.
+     * @param documentToUpdate o conteúdo do document a ser atualizado.
+     */
+    public static void updateDocumentByQuery(String atributeName, String atributeValue, String collectionName, Document documentToUpdate){
+        MongoCollection<Document> collection = getCollection(collectionName);
+        collection.updateOne(eq(atributeName, atributeValue), documentToUpdate);
+    }
+
+    /**
+     * Exclui um Document do banco de dados.
+     *
+     * @param atributeName nome do atributo no banco de dados desejado.
+     * @param atributeValue valor do atributo que será excluído no banco de dados.
+     * @param collectionName nome da collection que terá um document excluído.
+     * @return {@code true} se foi excluído com sucesso, {@code false} caso contrário.
+     */
+    public static boolean deleteDocument(String atributeName, String atributeValue, String collectionName) {
+        MongoCollection<Document> collection = getCollection(collectionName);
+        DeleteResult result = collection.deleteOne(eq(atributeName, atributeValue));
+        return result.getDeletedCount() > 0;
     }
 
     // *** Métodos privados ***
@@ -103,11 +166,11 @@ public class DBController {
     /**
      * Converte um JSON para Document.
      *
-     * @param jsonObject o JSON correspondente à uma classe DAO do SAEP.
+     * @param JSONdocument um documento no formato JSON qualquer.
      * @return o Document correspondente ao JSON passado por parâmetro.
      */
-    private static Document parseJsonToDocument(String jsonObject) {
-        return Document.parse(jsonObject);
+    private static Document parseJSON(String JSONdocument) {
+        return Document.parse(JSONdocument);
     }
 
     /**
